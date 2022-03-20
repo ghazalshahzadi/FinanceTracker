0321a288dcaf2cead62e456d1a14d082f077fd63
ghazalshahzadi <ghazalshahzadi1@gmail.com>
2022-03-20 21:20:52 +0500

    add stock price in browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
  
-  root 'welcome#index'
   
+  root 'welcome#index'
+   get 'my_portfolio', to:'users#my_portfolio'
   devise_for :users
+   get 'search_stock' , to:'stocks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
