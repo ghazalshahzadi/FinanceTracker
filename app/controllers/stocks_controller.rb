eaf1e28c64430e1d4acaaa08be7c6ddb764c60dd
ghazalshahzadi <ghazalshahzadi1@gmail.com>
2022-03-21 11:19:14 +0500

    Add javascript

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,22 @@
 class StocksController < ApplicationController
 def search
-    stock = Stock.new_lookup(params[:stock])
-    render json: stock
-    
-end
-
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+            format.js { render partial: 'users/result'}
+         end 
+         render json: 'users/my_portfolio'
+      else
+        respond_to do |format|
+        flash.now[:alert] = "Please enter a valid symbol to search"
+        format.js { render partial: 'users/result'}
+        end
+      end
+    else
+        respond_to do |format|
+            flash.now[:alert] = "Please enter a valid symbol to search"
+            format.js { render partial: 'users/result'}
+        end
+    end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,10 @@ class Stock < ApplicationRecord
         client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sanbox_api_key ],
                                       secret_token: 'Tsk_87db79820ca0411b9c5a46595a02df11',
                                        endpoint: 'https://sandbox.iexapis.com/v1')
-        client.price(ticker_symbol)
+        begin                                   
+           new(ticker: ticker_symbol , name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+         return nil
+        end 
     end
 end
